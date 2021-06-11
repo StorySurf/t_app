44aa8cfa1ab8f0164b0c3510775d99ffe9514f5d
espatatis <rohansamanta.org@gmail.com>
2021-06-11 20:05:42 +0530

    Added button notification

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ void main() => runApp(
 class MyApp extends StatelessWidget {
   // const MyApp({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,18 +17,57 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+
+class _HomePageState extends State<HomePage> {
+  FlutterLocalNotificationsPlugin flutterNotification;
+
+  @override
+  void initState() {
+    super.initState();
+    var androidInitialize = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOSInitialize = IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
+    flutterNotification = new FlutterLocalNotificationsPlugin();
+    flutterNotification.initialize(initializationSettings, onSelectNotification: notificationSelected);
+
+  }
+
+  Future _showNotification() async{
+    var androidDetails = new AndroidNotificationDetails("Channel ID", "Rohan Samanta", "This is my channel", importance: Importance.max);
+    var iOSDetails = IOSNotificationDetails();
+    var generalNotificationDetails = new NotificationDetails(android: androidDetails, iOS: iOSDetails);
+
+    await flutterNotification.show(0, "Task", "You created a task", generalNotificationDetails);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
             "App"
-          ),
         ),
-        body: Center(
-          child: Text(
-            "Hello Everyone"
+      ),
+      body: Center(
+          child: RaisedButton(
+            onPressed: _showNotification,
+            child: Text("Flutter Notification"),
           )
-        ),
       ),
     );
   }
+  Future notificationSelected(String payload) async {}
+
 }

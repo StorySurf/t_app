38b8dfc655c5e2692d78a6f832d89289102a1e9b
espatatis <rohansamanta.org@gmail.com>
2021-06-14 23:19:12 +0530

    Added scheduled notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 void main() => runApp(
   MyApp()
@@ -40,20 +42,41 @@ class _HomePageState extends State<HomePage> {
     var iOSInitialize = IOSInitializationSettings();
     var initializationSettings = new InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
     flutterNotification = new FlutterLocalNotificationsPlugin();
-    flutterNotification.initialize(initializationSettings, onSelectNotification: notificationSelected);
+    flutterNotification.initialize(initializationSettings, onSelectNotification: notificationSelected_1);
 
   }
 
-  Future _showNotification() async{
+  Future _showNotification(int i) async{
     var androidDetails = new AndroidNotificationDetails("Channel ID", "Rohan Samanta", "This is my channel", importance: Importance.max);
     var iOSDetails = IOSNotificationDetails();
     var generalNotificationDetails = new NotificationDetails(android: androidDetails, iOS: iOSDetails);
 
-    await flutterNotification.show(0, "Task", "You created a task", generalNotificationDetails);
+    // await flutterNotification.show(0, "Task", "You created a task", generalNotificationDetails);
+    var scheduledTime = DateTime.now().add(const Duration(seconds: 5));
+    tz.initializeTimeZones();
+    // tz.setLocalLocation(tz.getLocation('IN'));
+    for( var i = 0 ; i <= 1; i++ ) {
+      var k = i.toString();
+      await flutterNotification.zonedSchedule(
+          i,
+          'Checkout this story',
+          'Once upon a time...',
+          tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+          generalNotificationDetails,
+          androidAllowWhileIdle: true,
+          uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+          payload: k);
+    }
+
   }
+  int index = 1;
 
   @override
   Widget build(BuildContext context) {
+
+    _showNotification(index);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -61,19 +84,88 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Center(
-          child: RaisedButton(
-            onPressed: _showNotification,
-            child: Text("Flutter Notification"),
-          )
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  onPressed: () => _showNotification(index),
+                  child: Text("Flutter Notification"),
+                ),
+                InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondRoute()),
+                    );
+                  },
+                  child: Container(width: 300, height: 300, color: Colors.red),
+                ),
+                InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ThirdRoute()),
+                    );
+                  },
+                  child: Container(width: 300, height: 300, color: Colors.yellow),
+                ),
+              ],
+          ),
       ),
     );
   }
-  Future notificationSelected(String payload) async {
-    showDialog(context: context,
-    builder: (context) => AlertDialog(
-      content: Text("Notification clicked"),
-    ),
+  Future notificationSelected_1(String payload) async {
+    if(payload == "1"){
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ThirdRoute()),
+      );
+    }
+    else{
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SecondRoute()),
+      );
+    }
+
+  }
+
+  Future notificationSelected_2(String payload) async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SecondRoute()),
     );
   }
 
+
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Route"),
+      ),
+      body: Center(
+        child: Text("The turtle and the rabbit"),
+      ),
+    );
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: Text("Penguin and the polar bear"),
+      ),
+    );
+  }
 }
